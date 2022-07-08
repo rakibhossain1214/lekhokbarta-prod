c3f6b64d0ba7b1cd2979991a8c1cc9acfe72f4a6
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-08 21:52:30 +0600

    optimizeuniversaldefault turned off

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  experimental: {
-    optimizeUniversalDefaults: true,
-  },
+  // experimental: {
+  //   optimizeUniversalDefaults: true,
+  // },
   content: [
     './pages/**/*.js',
     './components/**/*.js',
