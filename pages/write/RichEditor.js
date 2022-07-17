4935acdf46fc05f7755a0ddc559fceb7141e4c12
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-17 21:25:31 +0600

    Added window inner height for mobile test

--- pages/write/RichEditor.js
+++ pages/write/RichEditor.js
@@ -33,7 +33,7 @@ let config = {
     'Times New Roman/Times New Roman, Times, serif;' +
     'Verdana',
   allowedContent: true,
-  height: 400,
+  height: typeof window !== 'undefined' ? window.innerHeight : '450',
   // plugins: "autogrow"
   // disableNativeSpellChecker: false
   // skin: "moono",
