44603eb5cd660ab33d8429b88b0c522ffff4e994
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-17 13:21:25 +0600

    last commit before trying another editor

--- pages/write/index.js
+++ pages/write/index.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import RichEditor from './RichEditor'
+import { addDoc, collection, getFirestore, doc, setDoc } from 'firebase/firestore'
 
 function Write() {
   const [editorData, setEditorData] = useState(null)
@@ -9,10 +10,19 @@ function Write() {
   }
 
   // console.log("EditorData: ", editorData);
+  const db = getFirestore()
+  const addData = () => {
+    //   addDoc(collection(db, 'content'), {
+    //     content: editorData
+    //   })
+    const cityRef = doc(db, 'content', 'test')
+    setDoc(cityRef, { editorData })
+  }
 
   return (
     <div>
       <RichEditor handleChange={handleChange} />
+      <button onClick={addData}>Add Data</button>
     </div>
   )
 }
