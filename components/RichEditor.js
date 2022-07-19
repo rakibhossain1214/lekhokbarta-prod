e16f27b5fd6324d44a5d6f9784872b62b72fcf8c
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-19 15:07:01 +0600

    Classic Suneditor with mobile view change

--- components/RichEditor.js
+++ components/RichEditor.js
@@ -1,36 +1,35 @@
 import React from 'react'
 import 'suneditor/dist/css/suneditor.min.css'
-import SunEditor, { buttonList } from 'suneditor-react'
+import SunEditor from 'suneditor-react'
 const defaultTheme = require('tailwindcss/defaultTheme')
 
-const defaultFonts = [
-  'Arial',
-  'Comic Sans MS',
-  'Courier New',
-  'Impact',
-  'Georgia',
-  'Tahoma',
-  'Trebuchet MS',
-  'Verdana',
-  'hello',
-]
-
 const CustomSunEditor = () => {
-  console.log(JSON.stringify(buttonList.complex))
   return (
     <SunEditor
       className="prose max-w-none"
       setOptions={{
-        buttonList: [
-          ['undo', 'redo'],
-          ['font', 'fontSize', 'formatBlock'],
-          ['align', 'list', 'outdent', 'indent'],
-          ['strike', 'subscript', 'superscript', 'horizontalRule', 'removeFormat'],
-          ['table', 'link', 'image', 'video'],
-          '/',
-          ['bold', 'underline', 'italic', 'fontColor', 'hiliteColor'],
-          ['fullScreen', 'showBlocks', 'preview', 'save'],
-        ],
+        buttonList:
+          screen.width >= 768
+            ? [
+                ['fullScreen', 'showBlocks'],
+                ['font', 'fontSize', 'formatBlock'],
+                ['bold', 'underline', 'italic', 'fontColor', 'hiliteColor'],
+                ['align', 'list', 'outdent', 'indent'],
+                ['strike', 'subscript', 'superscript', 'horizontalRule', 'removeFormat'],
+                ['link', 'table', 'image', 'video'],
+                ['undo', 'redo', 'save', 'preview'],
+              ]
+            : [
+                ['fullScreen', 'showBlocks'],
+                ['font', 'fontSize', 'formatBlock'],
+                ['align', 'list', 'outdent', 'indent'],
+                ['strike', 'subscript', 'superscript', 'horizontalRule', 'removeFormat'],
+                ['link', 'table', 'image', 'video'],
+                '/',
+                ['bold', 'underline', 'italic', 'fontColor', 'hiliteColor'],
+                ['undo', 'redo', 'save', 'preview'],
+              ],
+
         imageFileInput: false,
         minHeight: 300,
         defaultStyle: 'font-size:16px; font-family: Arial',
