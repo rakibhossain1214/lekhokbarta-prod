926e87ea9538a8dd74ed5ce5c591853f1a2fc002
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-25 00:10:40 +0600

    Dynamic Data load and save fixed

--- pages/write/[...slug].js
+++ pages/write/[...slug].js
@@ -1,54 +1,63 @@
-import React, { useState } from 'react'
-// import RichEditor from './RichEditor'
-// import SunEditor from '@/components/SunEditor'
-import { addDoc, collection, getFirestore, doc, setDoc } from 'firebase/firestore'
-import { getTestPost } from '@/lib/firestoreConnection'
+import React from 'react'
+import Link from '@/components/Link'
+import PageTitle from '@/components/PageTitle'
+import { getPostFrontMatterByUserIdAndPostId } from '@/lib/firestoreConnection'
+import { withProtected } from 'src/hook/route'
+import { getFirestore, doc, setDoc } from 'firebase/firestore'
 
 import dynamic from 'next/dynamic'
 const SunEditor = dynamic(() => import('@/components/SunEditor'), {
   ssr: false,
 })
 
-function CreateContent({ content }) {
+function CreateContent({ postData, auth }) {
+  const { user, logout } = auth
   const db = getFirestore()
-  const cityRef = doc(db, 'content', 'test')
-
-  const [editorData, setEditorData] = useState(null)
+  const postRef = doc(db, 'posts', postData.postId)
 
   const handleChange = (data) => {
-    setEditorData(data)
-
-    setDoc(cityRef, { editorData: data })
+    setDoc(postRef, { ...postData, content: data })
   }
 
-  // console.log("EditorData: ", editorData);
-
-  // const addData = () => {
-  //   //   addDoc(collection(db, 'content'), {
-  //   //     content: editorData
-  //   //   })
-  //   setDoc(cityRef, { editorData })
-  // }
+  if (postData?.authorDetails?.id !== user.uid) {
+    return (
+      <>
+        <div className="mt-24 text-center">
+          <PageTitle>
+            You dont have permission!{' '}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+          <Link href="/">
+            <button className="focus:shadow-outline-blue my-5 inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+              Back to homepage
+            </button>
+          </Link>
+        </div>
+      </>
+    )
+  }
 
   return (
     <div>
-      <SunEditor
-        handleChange={handleChange}
-        editorContent={content}
-        // saveData={saveData}
-      />
-      {/* <button onClick={addData}>Add Data</button> */}
+      <SunEditor handleChange={handleChange} editorContent={postData.content} />
     </div>
   )
 }
 
-export default CreateContent
+export default withProtected(CreateContent)
 
-export async function getServerSideProps() {
-  const content = await getTestPost()
-  // console.log(content)
+export async function getServerSideProps({ params }) {
+  if (params.slug[1] === undefined || params.slug[0] === undefined) {
+    return {
+      props: { postData: null },
+    }
+  }
+
+  const postData = await getPostFrontMatterByUserIdAndPostId(params.slug[0], params.slug[1])
   return {
-    props: { content: content !== undefined ? content : null },
+    props: { postData: postData !== undefined ? postData : null },
     // revalidate: 1
   }
 }
