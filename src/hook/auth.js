aac32758b2e3e81f6c9237d282cf43b54dbcd8ab
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-24 09:11:06 +0600

    scroll true after redirect

--- src/hook/auth.js
+++ src/hook/auth.js
@@ -1,18 +1,19 @@
 import { createContext, useContext, useState } from 'react'
 import AuthService from '../service/AuthService'
-import Router from 'next/router'
+import Router, { useRouter } from 'next/router'
 
 const authContext = createContext()
 
 export function AuthProvider(props) {
   const [user, setUser] = useState(null)
   const [error, setError] = useState('')
-  // const router = useRouter()
+  const router = useRouter()
+
   const loginWithGoogleRedirect = async () => {
     const { error, user } = await AuthService.loginWithGoogle()
     setUser(user ?? null)
     setError(error ?? '')
-    Router.push('/')
+    router.push('/', undefined, { scroll: true })
   }
 
   const loginWithGoogleNoRedirect = async () => {
@@ -24,7 +25,7 @@ export function AuthProvider(props) {
   const logout = async () => {
     await AuthService.logout()
     setUser(null)
-    Router.push('/')
+    router.push('/', undefined, { scroll: true })
   }
   const value = { user, error, loginWithGoogleNoRedirect, loginWithGoogleRedirect, logout, setUser }
 
