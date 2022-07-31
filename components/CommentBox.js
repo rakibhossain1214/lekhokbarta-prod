2e8b57f1cbac4c2cfe3b5d252c5e916713b7e83d
Rakib Hossain <rakibhossain1214@gmail.com>
2022-07-31 12:58:08 +0600

    CommentBox functions added

--- components/CommentBox.js
+++ components/CommentBox.js
@@ -1,40 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react'
 import { withPublic } from '../src/hook/route'
 
 function CommentBox({ auth }) {
-    const { user } = auth
+  const { user } = auth
+  const [commentText, setCommentText] = useState('')
+  const [buttonActive, setButtonActive] = useState(false)
 
-    if (user === null) {
-        return <>Please Log in to comment...</>
+  const handleChange = (e) => {
+    if (e.target.value !== '') {
+      setButtonActive(true)
+      setCommentText(e.target.value)
+    } else {
+      setButtonActive(false)
     }
-    return (
-        <div className="w-full bg-white dark:bg-gray-900 pt-3 mt-1 rounded shadow-lg">
+  }
 
-            <div className="flex ml-3">
-                <div className="mr-3">
-                    <img src={user.photoURL} alt="" className='rounded-full' style={{ width: '40px' }} />
-                </div>
-                <div>
-                    <h4 className="font-semibold">{user.displayName}</h4>
-                    <p className="text-xs text-gray-500">2 seconds ago</p>
-                </div>
+  const handleSubmit = () => {
+    document.getElementById('commentArea').value = ''
+    console.log(commentText)
+  }
 
-            </div>
-
-            <div className="mt-3 p-1 w-full">
-                <textarea rows="3" className="border p-2 rounded w-full dark:bg-gray-900" placeholder="Leave a comment..."></textarea>
-            </div>
-
-            <div className='flex justify-end'>
-                <div className="text-base font-medium leading-6">
-                    <button className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-                        Comment
-                    </button>
-                </div>
-            </div>
+  if (user === null) {
+    return <>Please Log in to comment...</>
+  }
+  return (
+    <div className="mt-1 w-full rounded bg-white pt-3 shadow-lg dark:bg-gray-900">
+      <div className="ml-3 flex">
+        <div className="mr-3">
+          <img src={user.photoURL} alt="" className="rounded-full" style={{ width: '40px' }} />
         </div>
+        <div>
+          <h4 className="font-semibold">{user.displayName}</h4>
+          <p className="text-xs text-gray-500">2 seconds ago</p>
+        </div>
+      </div>
 
-    );
+      <div className="mt-3 w-full p-1">
+        <textarea
+          id="commentArea"
+          rows="3"
+          onChange={handleChange}
+          className="w-full rounded border p-2 dark:bg-gray-900"
+          placeholder="Leave a comment..."
+        ></textarea>
+      </div>
+
+      {buttonActive ? (
+        <div className="flex justify-end">
+          <div className="text-base font-medium leading-6">
+            <button
+              onClick={handleSubmit}
+              className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+            >
+              Comment
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-end">
+          <div className="text-base font-medium leading-6">
+            <button
+              disabled
+              className="focus:shadow-outline-gray mx-1 inline rounded-lg border border-transparent bg-gray-300 px-4 py-1 text-sm font-medium leading-5 text-gray-500"
+            >
+              Comment
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
 }
 
-export default withPublic(CommentBox);
+export default withPublic(CommentBox)
