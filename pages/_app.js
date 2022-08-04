fa003dda01e2873446becf1d0a5395e7064023af
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-05 00:39:39 +0600

    codes updated for optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,22 @@
 import '../src/config/firebase.config'
-
 import '@/css/tailwind.css'
 import '@/css/prism.css'
-// import 'katex/dist/katex.css'
 import '@fontsource/inter/variable-full.css'
 import '@/css/suneditor.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
+// import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
+// import { ClientReload } from '@/components/ClientReload'
 
 import { AuthProvider } from '../src/hook/auth'
 import AuthStateChanged from '../src/layout/AuthStateChanged'
-import NextNProgress from "nextjs-progressbar";
+import NextNProgress from 'nextjs-progressbar'
 
-const isDevelopment = process.env.NODE_ENV === 'production'
-const isSocket = process.env.SOCKET
+// const isDevelopment = process.env.NODE_ENV === 'production'
+// const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
@@ -26,8 +24,8 @@ export default function App({ Component, pageProps }) {
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
-      {isDevelopment && isSocket && <ClientReload />}
-      <Analytics />
+      {/* {isDevelopment && isSocket && <ClientReload />} */}
+      {/* <Analytics /> */}
       <AuthProvider>
         <AuthStateChanged>
           <LayoutWrapper>
