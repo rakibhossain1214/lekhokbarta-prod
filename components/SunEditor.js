b7517e69b68d079e1b7a70e0d5b140ab444600fd
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-06 09:03:52 +0600

    Image compression added before upload

--- components/SunEditor.js
+++ components/SunEditor.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'suneditor/dist/css/suneditor.min.css'
 import SunEditor from 'suneditor-react'
 const defaultTheme = require('tailwindcss/defaultTheme')
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import Compressor from 'compressorjs'
 
 const storage = getStorage()
 const metadata = {
@@ -12,53 +13,62 @@ const metadata = {
 const CustomSunEditor = (props) => {
   function handleImageUploadBefore(files, info, uploadHandler) {
     const storageRef = ref(storage, 'images/' + props.postId + '/' + files[0].name)
-    const uploadTask = uploadBytesResumable(storageRef, files[0], metadata)
+    const image = files[0]
+    new Compressor(image, {
+      quality: 0.2, // 0.6 can also be used, but its not recommended to go below.
+      success: (compressedResult) => {
+        // compressedResult has the compressed file.
+        // Use the compressed file to upload the images to your server.
+        // setCompressedFile(compressedResult)
+        const uploadTask = uploadBytesResumable(storageRef, compressedResult, metadata)
 
-    uploadTask.on(
-      'state_changed',
-      (snapshot) => {
-        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
-        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-        console.log('Upload is ' + progress + '% done')
-        switch (snapshot.state) {
-          case 'paused':
-            console.log('Upload is paused')
-            break
-          case 'running':
-            console.log('Upload is running')
-            break
-        }
-      },
-      (error) => {
-        switch (error.code) {
-          case 'storage/unauthorized':
-            // User doesn't have permission to access the object
-            break
-          case 'storage/canceled':
-            // User canceled the upload
-            break
-          case 'storage/unknown':
-            // Unknown error occurred, inspect error.serverResponse
-            break
-        }
-      },
-      () => {
-        // Upload completed successfully, now we can get the download URL
-        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-          const response = {
-            // The response must have a "result" array.
-            result: [
-              {
-                url: downloadURL,
-                name: files[0].name,
-                size: files[0].size,
-              },
-            ],
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+            console.log('Upload is ' + progress + '% done')
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused')
+                break
+              case 'running':
+                console.log('Upload is running')
+                break
+            }
+          },
+          (error) => {
+            switch (error.code) {
+              case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break
+              case 'storage/canceled':
+                // User canceled the upload
+                break
+              case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break
+            }
+          },
+          () => {
+            // Upload completed successfully, now we can get the download URL
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              const response = {
+                // The response must have a "result" array.
+                result: [
+                  {
+                    url: downloadURL,
+                    name: files[0].name,
+                    size: files[0].size,
+                  },
+                ],
+              }
+              uploadHandler(response)
+            })
           }
-          uploadHandler(response)
-        })
-      }
-    )
+        )
+      },
+    })
   }
 
   return (
