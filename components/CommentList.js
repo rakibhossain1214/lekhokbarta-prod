ceb1da8c51c50f0b6975ae3a25c56bd9f6596b37
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-08 09:27:48 +0600

    load comment button fixed

--- components/CommentList.js
+++ components/CommentList.js
@@ -19,6 +19,7 @@ function CommentList({ postId, user, defaultPostData }) {
   const [buttonActive, setButtonActive] = useState(false)
   const [commentText, setCommentText] = useState('')
   const [voteError, setVoteError] = useState('')
+  const [loadComment, setLoadComment] = useState(false)
 
   const db = getFirestore()
   const postRef = doc(db, 'posts', postId)
@@ -26,6 +27,7 @@ function CommentList({ postId, user, defaultPostData }) {
   const [postData, setPostData] = useState(defaultPostData)
 
   useEffect(() => {
+    setLoadComment(false)
     setUpVoted(false)
     setDownVoted(false)
 
@@ -157,110 +159,116 @@ function CommentList({ postId, user, defaultPostData }) {
             )}
             <span> ( {postData.downVote?.length} ) </span>
           </button>
-          <button className="border-b border-teal-500 p-1">
-            Comments <span> ( {postData.comments.length} ) </span>
+          <button
+            className="border-b border-teal-500 p-1"
+            onClick={() => setLoadComment(!loadComment)}
+          >
+            {loadComment ? 'Hide Comments' : 'Load Comments'}
+            <span> ( {postData.comments.length} ) </span>
           </button>
         </p>
         <p className="ml-4 text-red-500">{voteError}</p>
-        {postData.comments.map((comment, i) => (
-          <div
-            key={i++}
-            className="bg-white-500 w-full rounded border-b pt-3 shadow-lg dark:bg-stone-900"
-          >
-            <div className="ml-3 flex">
-              <div className="mr-3">
-                <Image
-                  src={comment?.photoURL}
-                  width="38px"
-                  height="38px"
-                  alt="avatar"
-                  className="h-10 w-10 rounded-full"
-                />
-              </div>
-              <div>
-                <h4 className="font-semibold">{comment?.displayName}</h4>
-                <p className="text-xs text-gray-500">
-                  <Moment fromNow>{comment.modifiedAt}</Moment>
-                </p>
-              </div>
-              {comment?.uid === user?.uid ? (
-                <CommentOptions
-                  onEdit={onOptionsEdit}
-                  onDelete={onOptionsDelete}
-                  postData={postData}
-                  commentId={i}
-                />
-              ) : (
-                ''
-              )}
-            </div>
 
-            <div className="mt-1 w-full p-1">
-              {comEditId === i && editable ? (
-                <div className="mt-1 w-full p-1">
-                  <textarea
-                    id="commentArea"
-                    rows="3"
-                    onChange={handleChangeComment}
-                    className="w-full rounded border p-2 dark:bg-gray-900"
-                    placeholder="Leave a comment..."
-                    defaultValue={comment.commentText}
-                  ></textarea>
-
-                  {buttonActive ? (
-                    <div className="flex justify-end">
-                      <div className="text-base font-medium leading-6">
-                        <button
-                          onClick={handleUpdate}
-                          className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-                        >
-                          Update
-                        </button>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <button
-                          onClick={() => {
-                            setButtonActive(false)
-                            setEditable(false)
-                          }}
-                          className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="flex justify-end">
-                      <div className="text-base font-medium leading-6">
-                        <button
-                          disabled
-                          className="focus:shadow-outline-gray mx-1 inline rounded-lg border border-transparent bg-gray-300 px-4 py-1 text-sm font-medium leading-5 text-gray-500"
-                        >
-                          Update
-                        </button>
-                      </div>
-                      <div className="text-base font-medium leading-6">
-                        <button
-                          onClick={() => {
-                            setButtonActive(false)
-                            setEditable(false)
-                          }}
-                          className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
-                        >
-                          Cancel
-                        </button>
-                      </div>
-                    </div>
-                  )}
+        {loadComment &&
+          postData.comments.map((comment, i) => (
+            <div
+              key={i++}
+              className="bg-white-500 w-full rounded border-b pt-3 shadow-lg dark:bg-stone-900"
+            >
+              <div className="ml-3 flex">
+                <div className="mr-3">
+                  <Image
+                    src={comment?.photoURL}
+                    width="38px"
+                    height="38px"
+                    alt="avatar"
+                    className="h-10 w-10 rounded-full"
+                  />
                 </div>
-              ) : (
-                <p className="pl-2 pb-4 text-sm text-gray-800 dark:text-gray-200">
-                  {comment.commentText}
-                </p>
-              )}
+                <div>
+                  <h4 className="font-semibold">{comment?.displayName}</h4>
+                  <p className="text-xs text-gray-500">
+                    <Moment fromNow>{comment.modifiedAt}</Moment>
+                  </p>
+                </div>
+                {comment?.uid === user?.uid ? (
+                  <CommentOptions
+                    onEdit={onOptionsEdit}
+                    onDelete={onOptionsDelete}
+                    postData={postData}
+                    commentId={i}
+                  />
+                ) : (
+                  ''
+                )}
+              </div>
+
+              <div className="mt-1 w-full p-1">
+                {comEditId === i && editable ? (
+                  <div className="mt-1 w-full p-1">
+                    <textarea
+                      id="commentArea"
+                      rows="3"
+                      onChange={handleChangeComment}
+                      className="w-full rounded border p-2 dark:bg-gray-900"
+                      placeholder="Leave a comment..."
+                      defaultValue={comment.commentText}
+                    ></textarea>
+
+                    {buttonActive ? (
+                      <div className="flex justify-end">
+                        <div className="text-base font-medium leading-6">
+                          <button
+                            onClick={handleUpdate}
+                            className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+                          >
+                            Update
+                          </button>
+                        </div>
+                        <div className="text-base font-medium leading-6">
+                          <button
+                            onClick={() => {
+                              setButtonActive(false)
+                              setEditable(false)
+                            }}
+                            className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                    ) : (
+                      <div className="flex justify-end">
+                        <div className="text-base font-medium leading-6">
+                          <button
+                            disabled
+                            className="focus:shadow-outline-gray mx-1 inline rounded-lg border border-transparent bg-gray-300 px-4 py-1 text-sm font-medium leading-5 text-gray-500"
+                          >
+                            Update
+                          </button>
+                        </div>
+                        <div className="text-base font-medium leading-6">
+                          <button
+                            onClick={() => {
+                              setButtonActive(false)
+                              setEditable(false)
+                            }}
+                            className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500"
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                ) : (
+                  <p className="pl-2 pb-4 text-sm text-gray-800 dark:text-gray-200">
+                    {comment.commentText}
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <CommentBox postId={postId} postData={postData} user={user} />
     </>
