6443815ada544387c50a048b22f6376e7c19e098
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-08 10:25:57 +0600

    comment delete modal fixed for dark theme

--- components/CommentOptions.js
+++ components/CommentOptions.js
@@ -85,10 +85,10 @@ export default function Example({ commentId, onEdit, onDelete }) {
           <div className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden outline-none focus:outline-none">
             <div className="relative my-6 mx-6 w-auto max-w-3xl">
               {/*content*/}
-              <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
+              <div className="relative  flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none dark:bg-gray-800">
                 {/*header*/}
                 <div className="flex items-start justify-between rounded-t border-b border-solid border-slate-200 p-5">
-                  <h3 className="text-3xl font-semibold">Delete Comment</h3>
+                  <h3 className="text-3xl font-semibold dark:text-slate-300">Delete Comment</h3>
                   <button
                     className="float-right ml-auto border-0 bg-transparent p-1 text-3xl font-semibold leading-none text-black opacity-5 outline-none focus:outline-none"
                     onClick={() => setShowModal(false)}
@@ -100,12 +100,12 @@ export default function Example({ commentId, onEdit, onDelete }) {
                 </div>
                 {/*body*/}
                 <div className="relative flex-auto p-6">
-                  <p className="my-4 text-lg leading-relaxed text-slate-500">
+                  <p className="my-4 text-lg leading-relaxed text-slate-500 dark:text-slate-300">
                     Are you sure you want to delete this comment?
                   </p>
                 </div>
                 {/*footer*/}
-                <div className="flex items-center justify-end rounded-b border-t border-solid border-slate-200 p-6">
+                <div className="flex items-center justify-end rounded-b p-6">
                   <button
                     className="mr-1 mb-1 bg-red-500 px-6 py-2 text-sm font-bold uppercase text-white outline-none transition-all duration-150 ease-linear focus:outline-none"
                     type="button"
