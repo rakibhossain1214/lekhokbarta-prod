f29838a36a4c71984349288031d3d4373a29ce1c
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-16 14:52:16 +0600

    firebase config comment removed

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -23,12 +23,8 @@
 //   }
 // }
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp, getApps } from 'firebase/app'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -40,5 +36,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-// Initialize Firebase
-initializeApp(firebaseConfig)
+if (!getApps.length) {
+  initializeApp(firebaseConfig)
+}
