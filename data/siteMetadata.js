078ac53a1edf453362d9122d0a3703ada8d034af
Rakib Hossain <rakibhossain1214@gmail.com>
2022-08-22 10:15:45 +0600

    made ga to ua

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -24,7 +24,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: 'G-796QE3FW4B', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'UA-238156085-1', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
