28c727d07cc1ff1ab9b48f62e5604fd091b9112c
A. M. Rakibul Hossain <amrakibulhossain@gmail.com>
2022-09-25 12:23:03 +0600

    blogs original entities added

--- layouts/BlogListLayout.js
+++ layouts/BlogListLayout.js
@@ -168,7 +168,7 @@ export default function ListLayout({ posts, initialDisplayPosts = [], pagination
 
                     <Link href={`/blog/${postId}/${slug}`}>
                       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                        Noteworthy technology acquisitions 2021
+                        {title}
                       </h5>
                     </Link>
 
@@ -180,10 +180,7 @@ export default function ListLayout({ posts, initialDisplayPosts = [], pagination
                     </div>
 
                     <Link href={`/blog/${postId}/${slug}`}>
-                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                        Here are the biggest enterprise technology acquisitions of 2021 so far, in
-                        reverse chronological order.
-                      </p>
+                      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{summary}</p>
                     </Link>
 
                     <div className="flex items-center">
