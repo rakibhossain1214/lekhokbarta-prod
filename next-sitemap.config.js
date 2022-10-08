981b74600d38fe52a446292f6480c5aaf53b6087
A. M. Rakibul Hossain <amrakibulhossain@gmail.com>
2022-10-09 01:08:34 +0600

    sitemap new

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,14 +3,40 @@
 const siteMetadata = require('./data/siteMetadata')
 module.exports = {
   siteUrl: siteMetadata.siteUrl,
+  exclude: ['/404'],
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'],
   robotsTxtOptions: {
-    // policies: [
-    //     { userAgent: "*", disallow: "/write/*" },
-    //     { userAgent: "*", allow: "/" }
-    // ],
-    additionalSitemaps: [`${siteMetadata.siteUrl}/server-sitemap.xml`],
+    policies: [
+      {
+        userAgent: '*',
+        disallow: ['/404'],
+      },
+      { userAgent: '*', allow: '/' },
+    ],
+    additionalSitemaps: [
+      `${siteMetadata.siteUrl}/sitemap.xml`,
+      `${siteMetadata.siteUrl}/server-sitemap.xml`,
+    ],
   },
   // exclude: ['/write/*'],
 }
+
+// const siteUrl = process.env.NEXT_PUBLIC_DOMAIN_URL;
+// module.exports = {
+//   siteUrl,
+//   exclude: ["/404"],
+//   generateRobotsTxt: true,
+//   robotsTxtOptions: {
+//     policies: [
+//       {
+//         userAgent: "*",
+//         disallow: ["/404"],
+//       },
+//       { userAgent: "*", allow: "/" },
+//     ],
+//     additionalSitemaps: [
+//       `${siteUrl}sitemap.xml`,
+//       `${siteUrl}server-sitemap.xml`,
+//     ],
+//   },
+// };
