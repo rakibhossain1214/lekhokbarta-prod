b5635e7fda17554b27366a88ebadda47b4849b2e
A. M. Rakibul Hossain <amrakibulhossain@gmail.com>
2022-10-08 11:26:44 +0600

    back to blogs link fixed

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -308,7 +308,7 @@ export default function PostLayout({
               </div>
               <div className="pt-4 xl:pt-8">
                 <Link
-                  href="/blog"
+                  href="/"
                   className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
                 >
                   &larr; Back to the blog
