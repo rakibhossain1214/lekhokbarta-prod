f71296232f8be1bff602b871fb87b6d18b85275a
A. M. Rakibul Hossain <amrakibulhossain@gmail.com>
2022-10-10 02:18:35 +0600

    setDoc to updateDoc

--- pages/write/[...slug].js
+++ pages/write/[...slug].js
@@ -3,7 +3,7 @@ import Link from '@/components/Link'
 import PageTitle from '@/components/PageTitle'
 import { getPostFrontMatterByUserIdAndPostId, getAllTagsArray, updatePost, updatePostDraft, updatePostPublish } from '@/lib/firestoreConnection'
 import { withProtected } from 'src/hook/route'
-import { getFirestore, doc, setDoc } from 'firebase/firestore'
+import { getFirestore, doc, updateDoc } from 'firebase/firestore'
 import Accordion from '@material-ui/core/Accordion'
 import AccordionSummary from '@material-ui/core/AccordionSummary'
 import AccordionDetails from '@material-ui/core/AccordionDetails'
@@ -29,6 +29,7 @@ import Compressor from 'compressorjs'
 import Image from '@/components/Image'
 import kebabCase from '@/lib/utils/kebabCase'
 import categories from '../../data/categories'
+import { updatePostSource } from '../../lib/firestoreConnection'
 
 const postValidationSchema = Yup.object().shape({
   title: Yup.string()
@@ -56,36 +57,10 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
   const [publishToast, setPublishToast] = useState(false)
   const [sourceToast, setSourceToast] = useState(false)
   const [postSource, setPostSource] = useState(postData?.referenceSources)
+  const [selectedTags, setSelectedTags] = useState(defaultTags)
 
   const db = getFirestore()
   const storage = getStorage()
-  let postRef = null
-
-  if (postData !== 'NODATA' && postData !== null) {
-    postRef = doc(db, 'posts', postData.postId)
-  }
-
-  if (postData?.authorDetails?.id !== user.uid || postData === 'NODATA') {
-    return (
-      <>
-        <div className="mt-24 text-center">
-          <PageTitle>
-            You dont have permission!{' '}
-            <span role="img" aria-label="roadwork sign">
-              🚧
-            </span>
-          </PageTitle>
-          <Link href="/">
-            <button className="focus:shadow-outline-blue my-5 inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-              Back to homepage
-            </button>
-          </Link>
-        </div>
-      </>
-    )
-  }
-
-  const [selectedTags, setSelectedTags] = useState(defaultTags)
 
   const handleChange = (newValue) => {
     let tagArray = []
@@ -93,14 +68,6 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
       tagArray.push({ label: kebabCase(node.label.toLowerCase()), value: kebabCase(node.value.toLowerCase()) })
     })
     setSelectedTags(tagArray)
-
-
-    if (postRef !== null) {
-      postData.frontMatter.tags = tagArray
-      postData.frontMatter.draft = true
-      postData.frontMatter.status = 'pending'
-      setDoc(postRef, { ...postData, lastmod: new Date().toString() })
-    }
   }
 
   function handleImageUploadBefore(files) {
@@ -145,18 +112,20 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
           },
           () => {
             // Upload completed successfully, now we can get the download URL
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-              const postRef = doc(db, 'posts', postData.postId)
-              postData.frontMatter.postThumbnail = downloadURL
-              postData.frontMatter.draft = true
-              postData.frontMatter.status = 'pending'
-              setDoc(postRef, {
-                ...postData
-              }).then(() => {
-                setImageLoad(false)
-                setPostThumbnail(downloadURL)
+            getDownloadURL(uploadTask.snapshot.ref)
+              .then((downloadURL) => {
+                const postRef = doc(db, 'posts', postData.postId)
+                updateDoc(postRef, {
+                  "frontMatter.status": 'pending',
+                  "frontMatter.draft": true,
+                  "frontMatter.postThumbnail": downloadURL,
+                  lastmod: new Date().toString()
+                })
+                  .then(() => {
+                    setImageLoad(false)
+                    setPostThumbnail(downloadURL)
+                  })
               })
-            })
           }
         )
       },
@@ -168,50 +137,85 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
     var fileRef = ref(storage, postThumbnail)
     deleteObject(fileRef)
       .then(() => {
-        postData.frontMatter.postThumbnail = ''
-        postData.frontMatter.draft = true
-        postData.frontMatter.status = 'pending'
-        setDoc(postRef, {
-          ...postData
-        }).then(() => {
-          setPostThumbnail('')
+        updateDoc(postRef, {
+          "frontMatter.status": 'pending',
+          "frontMatter.draft": true,
+          "frontMatter.postThumbnail": "",
+          lastmod: new Date().toString()
         })
+          .then(() => {
+            setPostThumbnail('')
+          })
       })
       .catch((error) => {
         // Uh-oh, an error occurred!
       })
   }
 
-  const handlePostDraft = () => {
-    updatePostDraft({ postData })
+  const handlePostDraft = async () => {
+    await updatePostDraft({ postData })
     setDraftToast(true)
     setTimeout(() => {
       setDraftToast(false)
     }, 3000);
   }
 
-  const handlePostPublish = () => {
-    updatePostPublish({ postData })
+  const handlePostPublish = async () => {
+    await updatePostPublish({ postData })
     setPublishToast(true)
     setTimeout(() => {
       setPublishToast(false)
     }, 3000);
   }
 
-  const handleSourceSave = (e) => {
+  const handleSourceSave = async (e) => {
     e.preventDefault()
-    postData.referenceSources = postSource
-    updatePostDraft({ postData })
+    updatePostSource({ postData, postSource })
     setSourceToast(true)
     setTimeout(() => {
       setSourceToast(false)
     }, 3000);
   }
 
+
+  if (postData?.authorDetails?.id !== user.uid || postData === 'NODATA') {
+    return (
+      <>
+        <div className="mt-24 text-center">
+          <PageTitle>
+            You dont have permission!{' '}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+          <Link href="/">
+            <button className="focus:shadow-outline-blue my-5 inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+              Back to homepage
+            </button>
+          </Link>
+        </div>
+      </>
+    )
+  }
+
   return (
     <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <h5>Notes: Updating any field of your blog after publishing will lead to another review process.</h5>
       {/* <p>Did not submitted for approval!</p> */}
+
+      <div className='flex justify-end my-2 pt-2'>
+        <div className="text-base font-medium leading-6">
+          <button onClick={handlePostDraft} className="focus:shadow-outline-blue mx-1 inline rounded-lg border border-transparent bg-red-500 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+            Draft
+          </button>
+        </div>
+        <div className="text-base font-medium leading-6">
+          <button onClick={handlePostPublish} className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
+            Submit for Approval
+          </button>
+        </div>
+      </div>
+
       <div className='flex justify-start my-2 pt-6'>
         <div className="text-base font-medium leading-6">
           {
@@ -234,7 +238,19 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
                   </>
                 </div>
                 :
-                ''
+                <button className='flex items-start bg-teal-500 p-1 rounded text-gray-100 mt-2'>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
+                  </svg>
+                  <input className="cursor-pointer absolute opacity-0 block pin-r pin-t" type="file" style={{ width: '145px', height: '32px' }}
+                    accept="image/png, image/jpeg, image/jpg"
+                    onChange={(event) => {
+                      handleImageUploadBefore(event.target.files)
+                    }}
+                  />
+                  Add thumbnail
+                </button>
               :
               <div style={{ height: '80px', width: '120px' }} className="flex justify-items-center align-items-center">
                 <svg aria-hidden="true" className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -242,49 +258,16 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
                   <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                 </svg>
               </div>
-
           }
-          <button className='flex items-start bg-teal-500 p-1 rounded text-gray-100 mt-2'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 mr-1">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z" />
-              <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z" />
-            </svg>
-            <input className="cursor-pointer absolute opacity-0 block pin-r pin-t" type="file" style={{ width: '145px', height: '32px' }}
-              accept="image/png, image/jpeg, image/jpg"
-              onChange={(event) => {
-                handleImageUploadBefore(event.target.files)
-              }}
-            />
-            Add thumbnail
-          </button>
         </div>
       </div>
-      <div className='flex justify-end my-2 pt-2'>
-        <div className="text-base font-medium leading-6">
-          <button onClick={handlePostDraft} className="focus:shadow-outline-blue mx-1 inline rounded-lg border border-transparent bg-red-500 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-            Draft
-          </button>
-        </div>
-        <div className="text-base font-medium leading-6">
-          <button onClick={handlePostPublish} className="focus:shadow-outline-teal mx-1 inline rounded-lg border border-transparent bg-teal-600 px-4 py-1 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-            Submit for Approval
-          </button>
-        </div>
-      </div>
+
 
       <SunEditor postData={postData} editorContent={postData.content} />
 
       <div className="flex justify-center">
         <div className="mb-3 mt-3 w-full">
-          <div className='flex items-center justify-between'>
-            <label for="exampleFormControlTextarea1" className="form-label inline-block mb-2 mr-2 text-gray-700 pt-2">Sources</label>
-            <button className='text-gray-100 flex bg-teal-500 rounded mr-2' onClick={handleSourceSave}>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="m-1 w-5 h-5">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
-              </svg>
-              <span className='mr-2'>Save</span>
-            </button>
-          </div>
+          <label for="exampleFormControlTextarea1" className="form-label inline-block mb-2 mr-2 text-gray-700 pt-2">Sources</label>
           <textarea
             className="
               form-control
@@ -309,6 +292,14 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
             value={postSource}
             onChange={(e) => setPostSource(e.target.value)}
           ></textarea>
+          <div className='grid justify-items-end p-2'>
+            <button className='text-gray-100 flex bg-teal-500 rounded mr-2 p-1' onClick={handleSourceSave}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="m-1 w-5 h-5">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z" />
+              </svg>
+              <span className='mr-2'>Save sources</span>
+            </button>
+          </div>
         </div>
       </div>
 
@@ -363,7 +354,7 @@ function CreateContent({ postData, auth, tagsOptions, defaultTags }) {
                     >
                       <option value="">Select a category</option>
                       {
-                        categories.map((item)=>(
+                        categories.map((item) => (
                           <option value={item.value}>{item.label}</option>
                         ))
                       }
